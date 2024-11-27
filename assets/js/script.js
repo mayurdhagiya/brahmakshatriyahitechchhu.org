fd538744e2e026ac5c690641a0dca7aabdf2f306
Mayur Dhagiya <mayurdhagiya@gmail.com>
2024-11-27 17:32:27 +0530

    some beautification

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
+/**
+ * Opens a file in a new browser tab.
+ * @param {string} path - The file path to open.
+ */
 function openFile(path) {
     if (path) {
-        window.open(path, '_blank'); // Opens in a new tab
+        window.open(path, '_blank'); // Open the file in a new tab
     } else {
-        alert('File not found!');
+        alert('File not found!'); // Show an alert if the path is invalid
     }
 }
