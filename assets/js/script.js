7f6f321a63a1d43107c3d251b2a4ba54ede28ba7
Mayur Dhagiya <mayurdhagiya@gmail.com>
2024-11-27 16:16:19 +0530

    some changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
-function openFile(filePath) {
-    window.open(filePath, '_blank');
+function openFile(path) {
+    if (path) {
+        window.open(path, '_blank'); // Opens in a new tab
+    } else {
+        alert('File not found!');
+    }
 }
